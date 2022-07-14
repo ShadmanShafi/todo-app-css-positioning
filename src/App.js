3ae19553159f0b5657d4e08907ff288aade8c082
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2022-07-14 21:11:17 +0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage.js";
+import Dashboard from "./components/Dashboard.js";
+import CreateTodo from "./components/CreateTodo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create" element={<CreateTodo />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/CreateTodo.js
@@ -0,0 +1,7 @@
+export default function CreateTodo() {
+  return (
+    <>
+      <h1>CreateTodo</h1>;
+    </>
+  );
+}
